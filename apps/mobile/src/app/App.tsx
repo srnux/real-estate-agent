576173bbc866f7768618156bdb42faec8064b9e8
srnux <luka.zmikic@gmail.com>
2023-04-22 22:40:19 +0200

    choose image implemented

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -12,9 +12,11 @@ import {
   Image,
 } from 'react-native';
 import {
+  ImageLibraryOptions,
   ImagePickerResponse,
   MediaType,
   launchCamera,
+  launchImageLibrary,
 } from 'react-native-image-picker';
 import Svg, { Path } from 'react-native-svg';
 
@@ -48,6 +50,41 @@ export const App = () => {
     console.log(result);
     setImage(result);
   }
+
+  async function chooseImage(type: MediaType)  {
+    const options = {
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+    }as ImageLibraryOptions;
+    const response = await launchImageLibrary(options,);
+
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        alert('User cancelled camera picker');
+        return;
+      } else if (response.errorCode == 'camera_unavailable') {
+        alert('Camera not available on device');
+        return;
+      } else if (response.errorCode == 'permission') {
+        alert('Permission not satisfied');
+        return;
+      } else if (response.errorCode == 'others') {
+        alert(response.errorMessage);
+        return;
+      }
+      // console.log('base64 -> ', response.base64);
+      // console.log('uri -> ', response.uri);
+      // console.log('width -> ', response.width);
+      // console.log('height -> ', response.height);
+      // console.log('fileSize -> ', response.fileSize);
+      // console.log('type -> ', response.type);
+      // console.log('fileName -> ', response.fileName);
+      console.log(response);
+      setImage(response);
+  };
 
   function SelectImage() {
     if(selectedImage?.assets[0])
@@ -95,12 +132,12 @@ export const App = () => {
             <TouchableOpacity
               style={styles.whatsNextButton}
               onPress={() => {
-                takeImage('photo');
+                chooseImage('photo');
               }}
             >
               <SelectImage></SelectImage>
               <Text style={[styles.textMd, styles.textCenter]}>
-                Take image{/* {JSON.stringify(selectedImage)} */}
+                Choose image{/* {JSON.stringify(selectedImage)} */}
               </Text>
             </TouchableOpacity>
           </View>

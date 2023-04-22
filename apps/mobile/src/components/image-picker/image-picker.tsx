a0b535003fbd62ad261e571ad6a94410c6396894
srnux <luka.zmikic@gmail.com>
2023-04-22 22:59:40 +0200

    component created

---
+++ apps/mobile/src/components/image-picker/image-picker.tsx
@@ -0,0 +1,255 @@
+import React, { useState } from 'react';
+
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  ImageLibraryOptions,
+  ImagePickerResponse,
+  MediaType,
+  launchCamera,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+
+/* eslint-disable-next-line */
+export interface ImagePickerProps {}
+
+export function ImagePicker(props: ImagePickerProps) {
+  const [selectedImage, setImage] = useState<ImagePickerResponse>({
+    assets: [],
+    didCancel: false,
+    errorCode: undefined,
+    errorMessage: undefined,
+  });
+
+  async function takeImage(type: MediaType) {
+    const response = await launchCamera({
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+      videoQuality: 'low',
+      durationLimit: 30, //Video max duration in seconds
+      saveToPhotos: true,
+    });
+
+    if (response.didCancel) {
+      alert('User cancelled camera picker');
+      return;
+    } else if (response.errorCode === 'camera_unavailable') {
+      alert('Camera not available on device');
+      return;
+    } else if (response.errorCode === 'permission') {
+      alert('Permission not satisfied');
+      return;
+    } else if (response.errorCode === 'others') {
+      alert(response.errorMessage);
+      console.log(response.errorCode);
+      console.log(response.errorMessage);
+      return;
+    }
+
+    console.log(response);
+    setImage(response);
+  }
+
+  async function chooseImage(type: MediaType) {
+    const options = {
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+    } as ImageLibraryOptions;
+    const response = await launchImageLibrary(options);
+
+    console.log('Response = ', response);
+
+    if (response.didCancel) {
+      alert('User cancelled camera picker');
+      return;
+    } else if (response.errorCode === 'camera_unavailable') {
+      alert('Camera not available on device');
+      return;
+    } else if (response.errorCode === 'permission') {
+      alert('Permission not satisfied');
+      return;
+    } else if (response.errorCode === 'others') {
+      alert(response.errorMessage);
+      console.log(response.errorCode);
+      console.log(response.errorMessage);
+      return;
+    }
+    console.log(response);
+    setImage(response);
+  }
+
+  function SelectImage() {
+    if (selectedImage?.assets[0]) {
+      return (
+        <Image
+          source={{
+            uri: selectedImage?.assets[0] ? selectedImage.assets[0].uri : '',
+          }}
+          style={styles.imageStyle}
+        />
+      );
+    }
+    return <Text style={[styles.textMd, styles.textCenter]}>no image ...</Text>;
+  }
+
+  return (
+    <View style={[styles.section, styles.heroTitle]}>
+      <TouchableOpacity
+        style={[styles.whatsNextButton, styles.heroTitle]}
+        onPress={() => {
+          takeImage('photo');
+        }}
+      >
+        <SelectImage></SelectImage>
+        <Text style={[styles.textMd, styles.textCenter]}>
+          Take image{/* {JSON.stringify(selectedImage)} */}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.whatsNextButton}
+        onPress={() => {
+          chooseImage('photo');
+        }}
+      >
+        <SelectImage></SelectImage>
+        <Text style={[styles.textMd, styles.textCenter]}>
+          Choose image{/* {JSON.stringify(selectedImage)} */}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#ffffff',
+  },
+  codeBlock: {
+    backgroundColor: 'rgba(55, 65, 81, 1)',
+    marginVertical: 12,
+    padding: 12,
+    borderRadius: 4,
+  },
+  monospace: {
+    color: '#ffffff',
+    fontFamily: 'Courier New',
+    marginVertical: 4,
+  },
+  comment: {
+    color: '#cccccc',
+  },
+  marginBottomSm: {
+    marginBottom: 6,
+  },
+  marginBottomMd: {
+    marginBottom: 18,
+  },
+  marginBottomLg: {
+    marginBottom: 24,
+  },
+  textLight: {
+    fontWeight: '300',
+  },
+  textBold: {
+    fontWeight: '500',
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  text2XS: {
+    fontSize: 12,
+  },
+  textXS: {
+    fontSize: 14,
+  },
+  textSm: {
+    fontSize: 16,
+  },
+  textMd: {
+    fontSize: 18,
+  },
+  textLg: {
+    fontSize: 24,
+  },
+  textXL: {
+    fontSize: 48,
+  },
+  textContainer: {
+    marginVertical: 12,
+  },
+  textSubtle: {
+    color: '#6b7280',
+  },
+  section: {
+    marginVertical: 24,
+    marginHorizontal: 12,
+  },
+  shadowBox: {
+    backgroundColor: 'white',
+    borderRadius: 24,
+    shadowColor: 'black',
+    shadowOpacity: 0.15,
+    shadowOffset: {
+      width: 1,
+      height: 4,
+    },
+    shadowRadius: 12,
+    padding: 24,
+    marginBottom: 24,
+  },
+  listItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  listItemTextContainer: {
+    marginLeft: 12,
+    flex: 1,
+  },
+  appTitleText: {
+    paddingTop: 12,
+    fontWeight: '500',
+  },
+  hero: {
+    borderRadius: 12,
+    backgroundColor: '#143055',
+    padding: 36,
+    marginBottom: 24,
+  },
+  heroTitle: {
+    flex: 1,
+    // flexDirection: 'row',
+  },
+  heroTitleText: {
+    color: '#ffffff',
+    marginLeft: 12,
+  },
+  heroText: {
+    color: '#ffffff',
+    marginVertical: 12,
+  },
+  whatsNextButton: {
+    backgroundColor: '#ffffff',
+    paddingVertical: 16,
+    borderRadius: 8,
+    width: '50%',
+    marginTop: 24,
+  },
+  learning: {
+    marginVertical: 12,
+  },
+  love: {
+    marginTop: 12,
+    justifyContent: 'center',
+  },
+  imageStyle: {
+    width: 200,
+    height: 200,
+    margin: 5,
+  },
+});
+
+export default ImagePicker;
